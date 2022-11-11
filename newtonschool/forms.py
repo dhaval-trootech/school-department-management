d6ac3a1294557227092fe4846a4fa92e2338ce95
Dhaval Gojiya <dhaval.gojiya@trootech.com>
2022-11-11 19:10:00 +0530

    Final Saved Method After Data Modified

--- newtonschool/forms.py
+++ newtonschool/forms.py
@@ -3,6 +3,8 @@ from .models import School
 
 
 class SchoolModelForm(forms.ModelForm):
+
     class Meta:
         model = School
-        fields = "__all__"
+        fields = ['city', 'established_year', 'rating', 'owner_name']
+    city = forms.CharField(label="Name Your City")
--- newtonschool/views.py
+++ newtonschool/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render
 from .forms import SchoolModelForm
+from django.http import HttpResponse
 
 
 # Create your views here.
 
+# Method - 1
 def school_model_form(request):
-    context = {}
-    # create object of form
-    form = SchoolModelForm(request.POST)
-    # check if form data is valid
-    if form.is_valid():
-        # save the form data to model
-        form.save()
+    school_form = SchoolModelForm()
+    context = {'form': school_form}
+    if request.method == 'POST':
+        form = SchoolModelForm(request.POST)
+        if form.is_valid():
+            data = form.save(commit=False)
+            data.save()
+            return HttpResponse("data submitted successfully")
+    return render(request, "newtonschool/school_form.html", context)
 
-    context['form'] = form
-    return render(request, "index.html", context)
+
+# Method - 2
+# def school_model_form(request):
+#     form = SchoolModelForm()
+#     context = {'form': form}
+#     if request.method == 'POST':
+#         print("Before IS_VALID>:", request.POST)
+#         form.established_year = 999
+#         form.owner_name = request.POST['owner_name']
+#         form.rating = request.POST['rating']
+#         data = form.save(commit=False)
+#         # Finally write the changes into database
+#         data.save()
+#         return HttpResponse("data submitted successfully")
+#     return render(request, "newtonschool/school_form.html", context)

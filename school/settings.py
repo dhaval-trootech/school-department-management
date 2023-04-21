9db113b93ea97c74a810df9965bb89319fb1307a
Dhaval Trootech <dhaval.gojiya@trootech.com>
2023-04-21 12:35:05 +0530

    some change in settings

--- school/settings.py
+++ school/settings.py
@@ -28,48 +28,89 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', 'django.contrib.messages',
-                  'django.contrib.staticfiles', 'teachers', 'students', 'users', 'newtonschool', 'payments',
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'teachers',
+    'students',
+    'users',
+    'newtonschool',
+    'payments',
 
-                  ]
+]
 
-MIDDLEWARE = ['django.middleware.security.SecurityMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware',
-              'django.middleware.cache.UpdateCacheMiddleware', 'django.middleware.common.CommonMiddleware',
-              'django.middleware.cache.FetchFromCacheMiddleware', 'django.middleware.csrf.CsrfViewMiddleware',
-              'django.contrib.auth.middleware.AuthenticationMiddleware', 'django.contrib.messages.middleware.MessageMiddleware',
-              'django.middleware.clickjacking.XFrameOptionsMiddleware', ]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 ROOT_URLCONF = 'school.urls'
 
-TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates', 'DIRS': [BASE_DIR / 'templates', ], 'APP_DIRS': True, 'OPTIONS': {
-    'context_processors': ['django.template.context_processors.debug', 'django.template.context_processors.request',
-                           'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages', ], }, }, ]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates', ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'school.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3', }}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [{'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
-                            {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
-                            {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
-                            {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', }, ]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -91,9 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # MY CUSTOM SETTINGS ---->
 AUTH_USER_MODEL = 'users.SchoolUser'
 LOGIN_URL = '/users/login'
-
-CACHE_MIDDLEWARE_SECONDS = 20
-CACHES = {'default': {'BACKEND': 'django.core.cache.backends.db.DatabaseCache', 'LOCATION': 'school_caches'}}
 
 STRIPE_PUBLISHABLE_KEY = 'pk_test_51MHMDcSDtbLMILvbqC4GzZDruOEsb3DZnsCx5HkBHytxYFZUDQoKZGHzC0Xy4INvIXrm81QhlkgRqk1ho6DBSMVW00srWKPpcJ'
 STRIPE_SECRET_KEY = 'sk_test_51MHMDcSDtbLMILvbzTwJiSPYZmGwf64Q3onwP3zZQQJXoU00CPqONO1KVVuisg1hofDjZExsH5h3G8s9Yzo1ZEAj00XPIXupZs'

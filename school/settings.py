05c10a7c9d32c223fef712bf7aaa86e6db6017c4
Dhaval Trootech <dhaval.gojiya@trootech.com>
2024-10-14 17:12:32 +0530

    static root added

--- school/settings.py
+++ school/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "assets"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]

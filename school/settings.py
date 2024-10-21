d0ba3bc1b43b9152477af41d7e6efd03919cc1d5
Dhaval Trootech <dhaval.gojiya@trootech.com>
2024-10-21 14:18:58 +0530

    setup2

--- school/settings.py
+++ school/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 import environ
 
@@ -18,7 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Initialise environment variables :: django-environ ::
 env = environ.Env()
-environ.Env.read_env(BASE_DIR / ".env")
+
+# Read the .env file
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
